bac6739d500e22ffb1e2feaf0022d38f209c5bc2
rifansukarno-sys <rifansukarno@gmail.com>
2026-08-17 00:31:49 +0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  supabaseUrl: "https://atspcfinjykoyntitkfe.supabase.co",
+  supabaseKey: "sb_publishable_AY-BO4gmv_KdCaB2QjBMFw_hpc7sZvm",
+  redirectUrl: "https://rifansukarno-sys.github.io/my-learning-os/"
+};
