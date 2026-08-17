f788d26a044939711106a8c743448e83f62a90e6
rifansukarno-sys <rifansukarno@gmail.com>
2026-08-18 02:30:30 +0700

    Fix mobile Playwright project to Chromium device

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,6 @@ module.exports = defineConfig({
   },
   projects: [
     { name: 'desktop', use: { ...devices['Desktop Chrome'] } },
-    { name: 'mobile', use: { ...devices['iPhone 13'] } },
+    { name: 'mobile', use: { ...devices['Pixel 7'] } },
   ],
 });
